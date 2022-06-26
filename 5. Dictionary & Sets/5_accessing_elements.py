c21f08128524e09142d392abb77ea192c77c0d2f
Dhruv <dhruv.nitj@gmail.com>
2022-06-26 12:09:26 +0530

    jun26

--- 5. Dictionary & Sets/5_accessing_elements.py
+++ 5. Dictionary & Sets/5_accessing_elements.py
@@ -4,3 +4,15 @@ print(dict[1])
 print(dict['name'])
 
 # here dict would be a nested dictionary as name is a dictionary itself
+
+# accessing elements in a nested dictionary
+dict2 = {
+    1 : {1 : 'hello' , 2 : 'world'} ,
+    2 : {('hello', 1) , ('world' , 2)}
+}
+print()
+print(dict2[1])
+
+# accessing an element from a nested dictionary
+print()
+print(dict2[1][2])
