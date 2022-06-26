4d629a83f1d211b664c96e385f9d18efec7ff7fd
Dhruv <dhruv.nitj@gmail.com>
2022-06-26 17:20:58 +0530

    jun26

---
+++ 5. Dictionary & Sets/Part 3 - Practice Set/3_mcq.py
@@ -0,0 +1,8 @@
+s = {18, "18"}
+print(s)
+# we found that 18 and "18" are two diff things for set bcz both have diff datatypes
+
+s1= {18, 18.0 , "18", 18.01}
+print(len(s1))
+print(s1)
+# we foung that 18 and 18.0 are two same things for a set bcz of basic Python Rules
