42706271e24ba16c32aad1cc7eb39448e1dc4b58
Dhruv <dhruv.nitj@gmail.com>
2022-07-05 22:54:34 +0530

    july5

--- 10. File Handling in Python/5. Practice Set/ques02.py
+++ 10. File Handling in Python/5. Practice Set/ques02.py
@@ -1,13 +1,12 @@
 # Take an integer as input from user and if it is greater than the prev stored value in the file then update the file with the new integer
 
-n = int(input("Enter your Score: "))
-data = int(0)
+score = input("Enter your Score: ")
+
 with open('hiscore.txt', 'r') as file:
-    data = int(file.read())
+    hiscore = file.read()
 
-if (n > data):
-    with open('hiscore.txt', 'w') as file:
-        file.write(n)
-
-with open('hiscore.txt', r) as file:
-    print(file.read())
+if (score > hiscore):
+    with open ('hiscore.txt', 'w') as file:
+        file.write(score)
+else :
+    print("You did not score enough!")
