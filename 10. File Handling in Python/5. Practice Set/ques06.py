e077c207380cbd07f725b02a2d922e03d23f033b
Dhruv <dhruv.nitj@gmail.com>
2022-07-06 10:21:33 +0530

    july6

--- 10. File Handling in Python/5. Practice Set/ques06.py
+++ 10. File Handling in Python/5. Practice Set/ques06.py
@@ -1,1 +1,15 @@
 # search for the line number in which the word 'python' is present in log_file.txt
+content = True
+bool_flag = False
+i = 1
+with open("log_file.txt", "r") as file:
+    while (content):
+        content = file.readline()
+        if 'python' in content.lower():
+            bool_flag = True
+            print("Line Desc.:", content, end="")
+            print(f"Yes Python is present in line No. {i}.")
+            i += 1
+
+if (bool_flag == False):
+    print("No, Python was not present in the file.")
