6f6a0947c3927931d6689ce4c1bdb910637e9601
Dhruv <dhruv.nitj@gmail.com>
2022-07-06 09:46:28 +0530

    july6

---
+++ 10. File Handling in Python/5. Practice Set/ques05.py
@@ -0,0 +1,17 @@
+# Search for the word 'Python' in a log file.
+""" with open ("log_file.txt", "r") as file:
+    content = file.read()
+
+if ('Python' in content):
+    print("YES")
+else:
+    print("NO") """
+
+# The most standard way to open the file if you have to search a word is to open it in lower case, eg-
+with open ("log_file.txt", "r") as file:
+    content = file.read()
+
+if ('python' in content.lower()):
+    print("YES")
+else:
+    print("NO")
