41f3811672da4f552bebe298a2b273dba51e0ca7
Dhruv <dhruv.nitj@gmail.com>
2022-07-09 23:19:16 +0530

    july9

--- 11. OOPS/4. Practice Set/2_ques02.py
+++ 11. OOPS/4. Practice Set/2_ques02.py
@@ -1,8 +1,20 @@
 # write a class calculator capable of finding square, cube and square root of a number
+
 class calculator:
-    def printAfterCalc (self):
-        print(f"Square: {self*self}\tCube: {pow(self, 3)}\tSquare Root: {pow(self, 1/2)}")
+    def __init__(self, num):
+        self.number = num
+
+    def square (self):
+        print(f"Square: {self.number**2}")
+    def cube (self):
+        print(f"Cube: {pow(self.number, 3)}")
+    def squareRoot (self):
+        # print(f"Square Root: {pow(self.number, 0.5)}")
+        return pow(self.number, 0.5)
 
 number = int(input())
-number = calculator()
-number.printAfterCalc()
+a = calculator(number)
+a.square()
+a.cube()
+b = float(a.squareRoot())
+print("Square Root:",round(b, 2))
