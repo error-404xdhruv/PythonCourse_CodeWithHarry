568bd91c642ed2ac470fded3efc720bf4239bdf9
Dhruv <dhruv.nitj@gmail.com>
2022-07-24 23:03:44 +0530

    24

--- 14. Exception Handling/6. Practice Set/prob4.py
+++ 14. Exception Handling/6. Practice Set/prob4.py
@@ -1,3 +1,7 @@
+'''
+Write a program to display a/b where a and b are integers. If b=a, display infinte by handling the ZeroDivisionError.
+'''
+
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 try:
