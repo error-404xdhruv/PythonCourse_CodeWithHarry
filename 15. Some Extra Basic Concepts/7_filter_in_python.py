70c9da9ca320731ed2817bd77716c928b10dd645
Dhruv <dhruv.nitj@gmail.com>
2022-07-25 20:20:03 +0530

    25

--- 15. Some Extra Basic Concepts/7_filter_in_python.py
+++ 15. Some Extra Basic Concepts/7_filter_in_python.py
@@ -1,8 +1,13 @@
+
+'''
+The filter() method filters the given sequence with the help of a function that tests each element in the sequence to be true or not.
+'''
+
 '''
 Write a function to filter the vowels from a list
 '''
 
-def isVowel (letter):
+def isVowel(letter):
     vowels = ['a', 'e', 'i', 'o', 'u']
     temp = letter.lower()
     if temp in vowels:
@@ -10,6 +15,7 @@ def isVowel (letter):
     else:
         return False
 
-list1  = ['h', 'a', 'g', 't', 'e', 'r']
+
+list1 = ['h', 'a', 'g', 't', 'e', 'r']
 res = filter(isVowel, list1)
 print(list(res))
