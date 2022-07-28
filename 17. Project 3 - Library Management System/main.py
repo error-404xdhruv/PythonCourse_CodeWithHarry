2def38b2d0a8169ea60d0725572af1c938f74f36
Dhruv <dhruv.nitj@gmail.com>
2022-07-28 22:05:07 +0530

    28

--- 17. Project 3 - Library Management System/main.py
+++ 17. Project 3 - Library Management System/main.py
@@ -4,19 +4,21 @@ class Library:
 
     def displayBooksList(self):
         for book in self.books:
-            print(f"\t * ", {book})
+            print(f"\t * {book}")
 
     def borrowBooks(self, bookName):
         if bookName in self.books:
             print(
                 f"Issue Successfully. Please return the book within 10 days and keep it safe.")
+            self.books.remove(bookName)
+            return True
         else:
             print("Sorry, the book you want is currently unavailable.")
+            return False
 
     def returnBook(self, bookName):
         self.books.append(bookName)
         print("Thank You.")
-
 
 class student:
     def requestBook(self):
@@ -24,8 +26,7 @@ class student:
         return self.book
 
     def returnBook(self):
-        self.book = input(
-            "Enter the name of the book you want to return or append: ")
+        self.book = input("Enter the name of the book you want to return or donate: ")
         return self.book
 
 
@@ -34,13 +35,14 @@ if __name__ == "__main__":
         welcomeMsg = '''
 -------- Welcome to Library Management System --------
     \tPlease choose an option:
-        1. List all the books.
+        1. List all the books
         2. Request a Book
         3. Donate / Return a book
         4. Exit the Library
         '''
         print(welcomeMsg)
-        CentralLibrary = Library(["Data Structures and Algo", "Django", "CLRS", "Python Notes"])
+        CentralLibrary = Library(
+            ["Data Structures and Algo", "Django", "CLRS", "Python Notes"])
         cmd = int(input("Enter your Choice: "))
         # since Python does not have switch case, we have   to use simple if else to go around
         if (cmd == 1):
@@ -51,5 +53,6 @@ if __name__ == "__main__":
             CentralLibrary.returnBook(student.returnBook())
         elif (cmd == 4):
             print("Thanks for using our service.")
+            break
         else:
             print("Invalid Choice.")
